b3371b88488a4cbe8b0cc96251321428c023ba0e
pon <nandynandy.240799@gmail.com>
2024-09-13 12:11:16 +0530

    hai

---
+++ src/test/java/org/T/B.java
@@ -0,0 +1,14 @@
+package org.T;
+
+public class B {
+	private void company() {
+		System.out.println("compname");
+		}
+		private void sample() {
+		System.out.println("sampId");
+		}
+		private void employee() {
+		System.out.println("empId");
+		}
+		}
+
